cb2d8d93b0146704c2035a6709b131f966fd901b
Abdeljalil37 <widadpop@gmail.com>
2024-02-13 23:47:42 +0100

    Add top_ten function to query Reddit API

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Queries the Reddit API and returns the number """
+import requests
+
+def top_ten(subreddit):
+    """ Returns the number of subscribers for a given subreddit. """
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+        'AppleWebKit/537.36(KHTML, like Gecko)'
+        'Chrome/58.0.3029.110 Safari/537.36'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        print(None)
+        return
+
+    data = response.json().get('data').get('children')
+    for i in range(10):
+        print(data[i].get('data').get('title'))

f2ceee0edd7e6dbef2a4d0b3f7bd090ed43bb7ba
Abdeljalil37 <widadpop@gmail.com>
2024-02-13 17:11:21 +0100

    Refactor number_of_subscribers function to handle API response correctly

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-""" Queries the Reddit API and returns the number of subscribers for a given subreddit. """
+""" Queries the Reddit API and returns the number
+of subscribers for a given subreddit. """
 import requests
 
 
 def number_of_subscribers(subreddit):
+    """ Returns the number of subscribers for a given subreddit. """
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
 
     headers = {
@@ -11,6 +13,7 @@ def number_of_subscribers(subreddit):
         'AppleWebKit/537.36(KHTML, like Gecko)'
         'Chrome/58.0.3029.110 Safari/537.36'}
     response = requests.get(url, headers, allow_redirects=False)
-    if response.status_code != 200:
+    if response.status_code == 200:
+        return response.json().get('data').get('subscribers')
+    else:
         return 0
-    return response.json().get('data').get('subscribers')

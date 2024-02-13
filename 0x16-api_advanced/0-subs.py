4f02473c4de2be18e267ac146563dc5b864eb281
Abdeljalil37 <widadpop@gmail.com>
2024-02-13 20:27:39 +0100

    Refactor number_of_subscribers function to handle invalid subreddit input

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,14 +6,16 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ Returns the number of subscribers for a given subreddit. """
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-        'AppleWebKit/537.36(KHTML, like Gecko)'
-        'Chrome/58.0.3029.110 Safari/537.36'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    if type(subreddit) is not str or subreddit == "":
         return 0
     else:
-        return response.json().get('data').get('subscribers')
+        url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+            'AppleWebKit/537.36(KHTML, like Gecko)'
+            'Chrome/58.0.3029.110 Safari/537.36'}
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code != 200:
+            return 0
+        else:
+            return response.json().get('data').get('subscribers')
